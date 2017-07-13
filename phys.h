12c5e0f951b168df6f196b7715955e6dd7665f92
LoneLogical <awhit023@ucr.edu>
2017-07-13 15:47:24 -0700

    global variables and functions for mathematical equations

---
+++ phys.h
@@ -0,0 +1,27 @@
+// phys.h
+
+//===================
+// Include Guard
+#ifndef _PHYS_H_INCLUDED_
+#define _PHYS_H_INCLUDED_
+//===================
+// Forward declarations
+
+//====================
+// Include dependencies
+#include "loc.h"
+//=====================
+// Global Physics Constants
+
+
+
+//=====================
+// Global Physics Equations
+
+Force morse_Potent_Equ();
+Force linear_Spring_Equ();
+Force 
+
+
+//=====================
+#endif
